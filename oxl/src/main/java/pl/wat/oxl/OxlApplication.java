cc99682304d9cdbd37ecb91cc4d80630257782c3
Toby <t.waszkowiak@sages.com.pl>
2022-12-05 09:00:38 +0100

    Initial commit

---
+++ oxl/src/main/java/pl/wat/oxl/OxlApplication.java
@@ -0,0 +1,13 @@
+package pl.wat.oxl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxlApplication.class, args);
+	}
+
+}
---
+++ oxl/src/test/java/pl/wat/oxl/OxlApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wat.oxl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
